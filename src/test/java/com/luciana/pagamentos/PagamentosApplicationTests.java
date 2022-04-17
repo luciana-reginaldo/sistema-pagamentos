9b0a9f07eba78bb6e93e321332df0b7afc2b5c41
luciana-reginaldo <luciana.reginaldocps@gmail.com>
2022-04-17 20:06:20 -0300

    Commit inicial

---
+++ src/test/java/com/luciana/pagamentos/PagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciana.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
